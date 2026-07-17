9d0b09fa48776b0c71862b32ea438861a534d4ab
Finja111 <finja@marblestone.de>
2026-07-17 13:12:55 +0200

    Gibt jetzt ein ToDo/Tagebuch Dokument (lowkey nur für den ersten Tag sinnvoll, aber dass wird schon passen)

--- streamer-spiel/scenes/dokument.gd
+++ streamer-spiel/scenes/dokument.gd
@@ -3,12 +3,17 @@ extends Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	self.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if GameManager.current_stream_type == "offstream":
-		visible = false
-	else:
-		visible = true
+	pass
+
+
+func _on_quit_button_2_pressed() -> void:
+	self.visible = false
+
+
+func _on_t_dokument_button_pressed() -> void:
+	self.visible = true
